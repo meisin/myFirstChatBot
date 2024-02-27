e816341204405397fac12615f144b1861fb02e00
Dr. Meisin Lee <meisin123@gmail.com>
2024-02-27 13:47:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,14 @@ if prompt := st.chat_input(disabled= not(hf_email and hf_pass)):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
+
+# Generate a new response if last message is not from assistant 
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_response(promt, hf_email, hf_pass)
+            st.write(response)
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
     
 st.write('Hello world!')
